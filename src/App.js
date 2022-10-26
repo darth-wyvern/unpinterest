dc85f3db8a9f2cc9545044353e295432e0638d7f
darth-wyvern <nguyenquocnhan777@gmail.com>
2022-10-26 17:26:27 +0700

    update app component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,24 @@
-import React, { useEffect } from 'react';
-import ImageManagement from './image-management/ImageManagement';
-import AppHeader from './layout/AppHeader';
-import { Box } from '@chakra-ui/react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getImages, changePage } from './image-management/imageSlice';
-import './App.css'
-import AppPagination from './layout/AppPagination';
-import usePagination from './common/usePagination';
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import "./App.css";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import withSuspense from "./common/withSuspense";
+
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
+const signin = React.lazy(() => import('./Auth/Login'))
+const signup = React.lazy(() => import('./Auth/Register'))
 
 function App() {
-  const dispatch = useDispatch()
-  const { query, page } = useSelector(state => state.image)
-  const { currentPage, gotoPage, listNode, prev, next } = usePagination(page.totalPage)
-
-  useEffect(() => {
-    dispatch(getImages({ query, page }))
-    gotoPage(page.number)
-  }, [dispatch, gotoPage, page, query])
-
-  useEffect(() => {
-    gotoPage(page.number)
-  }, [gotoPage, page.number])
-
-  useEffect(() => {
-    dispatch(changePage(currentPage))
-  }, [currentPage, dispatch])
-
-
   return (
     <Box className="App" p={3}>
-      <Box pos='fixed' top={0} zIndex={2}>
-        <AppHeader />
-      </Box>
-      <Box >
-        <ImageManagement />
-        <Box pos='fixed' right={0} left={0} bottom={0} p={4} zIndex={2} bg='#0006' opacity='0.7' transition='.2s' _hover={{ opacity: 1 }}>
-          <AppPagination currentPage={currentPage} listNode={listNode} gotoPage={gotoPage} prev={prev} next={next} />
-        </Box>
-      </Box>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={withSuspense(DefaultLayout)()} >
+            <Route path="signin" element={withSuspense(signin)()} />
+            <Route path="signup" element={withSuspense(signup)()} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Box>
   );
 }
