037966fc886f4682b9772fb623d34ef757c21269
darth-wyvern <nguyenquocnhan777@gmail.com>
2022-10-26 17:25:42 +0700

    create withSuspense

---
+++ src/common/withSuspense.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function withSuspense(Component) {
+  return function SuspenseComponent(props) {
+    return (
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Component {...props} />
+      </React.Suspense>
+    );
+  };
+}
